62d3f672a7aebc27c97b47a9ba0ca821c8c6bd0d
dexfrost89 <dexfrost89@gmail.com>
2021-04-06 12:09:56 +0300

    Update interaction.py

--- softac/interaction.py
+++ softac/interaction.py
@@ -1,22 +1,19 @@
 from mlagents_envs.environment import UnityEnvironment
 from mlagents_envs.side_channel.engine_configuration_channel import EngineConfigurationChannel
+from gym_unity.envs import UnityToGymWrapper
 from random import  randint
 from mlagents_envs.base_env import ActionTuple
 import numpy as np
 
-env = UnityEnvironment(file_name="RobotBalance.x86_64", seed=1, side_channels=[])
 
-env.reset()
+env = UnityToGymWrapper(UnityEnvironment(file_name="RobotBalance.x86_64", seed=1, side_channels=[], no_graphics=True), allow_multiple_obs=False)
 
-behavior_names = env.behavior_specs.keys()
-print(list(behavior_names))
+#behavior_names = env.behavior_specs.keys()
+#print(list(behavior_names))
 
-for i in range(10000):
-    env.reset()
+for i in range(10):
+    obs = env.reset()
     for j in range(400):
-        act = ActionTuple(continuous=np.array([randint(-1, 1) for k in range(20)]).reshape(-1, 20))
-
-        env.set_actions('Spider?team=0', act)
-        env.step()
-        stepd = env.get_steps('Spider?team=0')
-        print(stepd[0].reward)
+        act = np.array([randint(-1, 1) for k in range(20)], dtype=np.float).reshape(-1, 20)
+        obs, reward, done, info = env.step(act)
+        print(obs.shape)
